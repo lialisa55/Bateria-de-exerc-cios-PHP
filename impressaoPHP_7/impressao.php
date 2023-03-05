9082bb86f7c5986f5ae3c1819380f68eb05fecb2
lialisa55 <nathalia.mota@etec.sp.gov.br>
2023-03-05 10:17:39 -0300

    adicionado exercício 7 'impressão php', sem testes

---
+++ impressaoPHP_7/impressao.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Imprimir números</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1>Imprimindo 100 vezes 'PHP'</h1>
+        <?php
+            for ($i = 0; $i < 100; $i++){
+                echo $i;
+                echo "   "
+            }
+        ?>
+    </body>
+</html>
