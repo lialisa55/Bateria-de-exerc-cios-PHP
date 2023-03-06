2b9c161f30296552f6e56136de1c514437e9e4b5
lialisa55 <nathalia.mota@etec.sp.gov.br>
2023-03-05 21:21:31 -0300

    adicionado imagens para triângulos, sem testes

--- triangulo_12/triangulo.php
+++ triangulo_12/triangulo.php
@@ -11,15 +11,15 @@
             $ladoC = $_GET['ladoUserC'];
 
             if($ladoA + $ladoB > $ladoC > $ladoA - $ladoC || $ladoB + $ladoC > $ladoA > $ladoB - $ladoC || $ladoA + $ladoC > $ladoB > $ladoA - $ladoC){
-                echo "É um triângulo "
+                echo "É um triângulo, pois um dos lados é menor que os dois outros juntos mas maior que a diferença entre esses outros."
                 if ($ladoA != $ladoB != $ladoC){
-                    echo "escaleno"
+                    echo "<img src='style/escaleno.png' alt='Triângulo escaleno'/> Esse triângulo é classificado como escaleno, pois todos os lados são diferentes"
                 }
                 if ($ladoA == $ladoB == $ladoC){
-                    echo "equilátero"
+                    echo "<img src='style/equilatero.png' alt='Triângulo equilátero'/> Esse triângulo é classificado como equilátero, pois todos os lados são iguais"
                 }
                 else{
-                    echo "isóceles"
+                    echo "<img src='style/isosceleso.png' alt='Triângulo isósceles'/> Esse triângulo é classificado como isósceles, pois dois lados são iguais e um é um diferente desses dois"
                 }
             }
             else {
